27a65d7d35f5f16005fa8de16a938de0111590ce
vanurag02 <anvaidya6@gmail.com>
2026-04-18 09:59:20 +0530

    feat: Custom Error Page added | Apr 18, 2026

--- budget-app/src/pages/Error.jsx
+++ budget-app/src/pages/Error.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Link, useNavigate, useRouteError } from "react-router-dom";
+
+// ICONS
+import { HomeIcon, ArrowUturnLeftIcon } from "@heroicons/react/24/solid";
 
 const Error = () => {
-  return <div>Error</div>;
+  const error = useRouteError();
+  const navigate = useNavigate();
+
+  return (
+    <div className="error">
+      <h1>Uh oh! We've got a problem</h1>
+      <p>{error.message || error.statusText}</p>
+      <div className="flex-md">
+        <button className="btn btn--dark" onClick={() => navigate(-1)}>
+          <span>Go Back</span>
+        </button>
+        <Link to="/" className="btn btn--dark">
+          <HomeIcon width={20} />
+          <span>Go to Home</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Error;
